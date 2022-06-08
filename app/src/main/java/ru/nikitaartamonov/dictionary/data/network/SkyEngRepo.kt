8e19e11d612a832d066a00a71b8cc01972094755
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-06-09 00:50:36 +0300

    separated operators in few lines

--- app/src/main/java/ru/nikitaartamonov/dictionary/data/network/SkyEngRepo.kt
+++ app/src/main/java/ru/nikitaartamonov/dictionary/data/network/SkyEngRepo.kt
@@ -10,22 +10,24 @@ class SkyEngRepo(private val skyEngApi: SkyEngApi) {
 
     fun getMeaning(word: String): Observable<WordMeaning> {
         val wordMeaning = PublishSubject.create<WordMeaning>()
-        skyEngApi.search(word).observeOn(AndroidSchedulers.mainThread()).subscribeBy(
-            onError = {
-                wordMeaning.onError(it)
-                wordMeaning.onComplete()
-            },
-            onSuccess = { meaningsList ->
-                if (meaningsList.isEmpty()) {
-                    wordMeaning.onError(Throwable("Nothing was found"))
+        skyEngApi.search(word)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onError = {
+                    wordMeaning.onError(it)
                     wordMeaning.onComplete()
-                } else {
-                    val meaning = meaningsList[0].meanings[0].translation.text
-                    wordMeaning.onNext(WordMeaning(meaning))
-                    wordMeaning.onComplete()
+                },
+                onSuccess = { meaningsList ->
+                    if (meaningsList.isEmpty()) {
+                        wordMeaning.onError(Throwable("Nothing was found"))
+                        wordMeaning.onComplete()
+                    } else {
+                        val meaning = meaningsList[0].meanings[0].translation.text
+                        wordMeaning.onNext(WordMeaning(meaning))
+                        wordMeaning.onComplete()
+                    }
                 }
-            }
-        )
+            )
         return wordMeaning
     }
 }
