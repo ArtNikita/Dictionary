146700ade31df24a769b81196f68919c15b173be
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-06-09 00:48:29 +0300

    extracted base url as const val

--- app/src/main/java/ru/nikitaartamonov/dictionary/Constants.kt
+++ app/src/main/java/ru/nikitaartamonov/dictionary/Constants.kt
@@ -1,3 +1,4 @@
 package ru.nikitaartamonov.dictionary
 
 const val DATA_BASE_NAME = "PairOfWordsDataBase"
+const val BASE_URL = "https://dictionary.skyeng.ru/api/public/v1/"
--- app/src/main/java/ru/nikitaartamonov/dictionary/data/di/DiStorage.kt
+++ app/src/main/java/ru/nikitaartamonov/dictionary/data/di/DiStorage.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.nikitaartamonov.dictionary.BASE_URL
 import ru.nikitaartamonov.dictionary.DATA_BASE_NAME
 import ru.nikitaartamonov.dictionary.data.network.SkyEngApi
 import ru.nikitaartamonov.dictionary.data.network.SkyEngRepo
@@ -39,7 +40,7 @@ object DiStorage {
 
     private val skyEngApi: SkyEngApi by lazy {
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://dictionary.skyeng.ru/api/public/v1/")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
